ee188b999ace587cc77c7f9beaca86c80145f39b
Flannel <54649204+FlannelDev@users.noreply.github.com>
2020-01-07 16:01:20 -0500

    Removed older Lua Scripts

--- Scripts #move this before compile/cultist_stalker.lua
+++
@@ -1,7 +0,0 @@
-
-function spawn_stalker(event, creature)
-    creature:AddAura(39947, creature)
-    creature:SetScale(2)
-end
-
-RegisterCreatureEvent(50094, 5, spawn_stalker)
--- Scripts #move this before compile/guardian_banner_regen.lua
+++
@@ -1,9 +0,0 @@
-local NPC_ID = 50053
-
-function spawn(event, creature)
-    creature:AddAura(500266, creature)
-        creature:AddAura(42716, creature)
-creature:SetMaxHealth(250 + 110 * creature:GetLevel())
-end
-
-RegisterCreatureEvent(NPC_ID, 5, spawn)
